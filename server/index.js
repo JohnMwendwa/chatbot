e283a6c88b392b7f3b9875fc339d8b7b0bac34b7
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-25 09:04:26 +0300

    setup server to listen and accept GET and POST calls

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const socketio = require('socket.io');
+const {v4:uuid} = require('uuid')
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
-const PORT = process.env.PORT || 3000;
+const io = socketio(server,{
+    cors:{
+        origin:'http://localhost:3000',
+        methods:['GET','POST']
+    }
+});
+const PORT = process.env.PORT || 3001;
 app.use(cors)
 
 
