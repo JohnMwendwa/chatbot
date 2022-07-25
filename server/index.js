30d66ce44150fd7ab4f207f28789d1418d7cfa89
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-25 11:31:12 +0300

    enable users to join specific chatrooms

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const socketio = require('socket.io')
 const {generateMessage} = require('./utils/message')
+const {addUser,removeUser,getUser,getUsersInRoom} = require('./utils/users')
 
 const app = express();
 const server = http.createServer(app);
@@ -16,10 +17,22 @@ const PORT = process.env.PORT || 3001;
 app.use(cors)
 
 io.on('connection',(socket)=>{
-    socket.emit('message',generateMessage('welcome'))
+    socket.on('join',(userDetails,callback)=>{
+        const {error,user} = addUser({id:socket.id,...userDetails})
+        
+        if(error){
+          return callback(error)
+        }
+
+        socket.join(user.room)
+        socket.emit('message',generateMessage('welcome'))
+        socket.broadcast.to(user.room).emit(`${user.username} has joined the chat`)
+        callback()
+    })
+
     socket.on('newMessage',(data)=>{
         socket.emit('message',generateMessage(data))
-    })
+    })   
 })
 
 server.listen(PORT,()=>{
