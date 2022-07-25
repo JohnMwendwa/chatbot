0d44ef4f7987163c4c460411d9ddb063a6312f5d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-25 09:32:01 +0300

    setup a basic form input and listen for messages from the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
+import {useEffect,useState} from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3001')
 
 function App() {
+const [message,setMessage] = useState([]);
+const [newMessage,setNewMessage] = useState('')
+console.log(message)
+    useEffect(()=>{
+      socket.on('message',(data)=>{
+        setMessage([...message,data])
+      })
+    },[message])
+ 
+    const sendMessage =(e)=>{
+        e.preventDefault();
+        socket.emit('newMessage',newMessage)
+        setNewMessage('')
+    }
+
   return (
     <div className="App">
-  
+      <h2>Messages</h2>
+        <ul>
+          {message.map(msg=><li key={msg.id}>{msg.msg}</li>)}
+        </ul>
+        <form onSubmit={sendMessage}>
+          <input 
+           type="text" 
+           value={newMessage}
+           onChange={(e)=>setNewMessage(e.target.value)}
+          />
+         <button>Send</button>
+        </form>
     </div>
   );
 }
