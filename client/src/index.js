68d90d43b34b24ad854310cc51711f7c5ec2c0b7
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-25 15:27:26 +0300

    wrap the App component with the context provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import {ConnectionProvider} from './context/connectionContext.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
+      <ConnectionProvider>
+          <App />
+      </ConnectionProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
