41ce9de21d250f44c0a961171094a1f28216f325
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-25 21:02:59 +0300

    import NewMessageForm component and render it

--- client/src/components/ChatRoom.js
+++ client/src/components/ChatRoom.js
@@ -1,11 +1,11 @@
 import {useEffect,useState,useContext} from 'react';
 import {ConnectionContext} from '../context/connectionContext.js';
 import Message from './Message.js';
+import NewMessageForm from './NewMessageForm.js';
 
 function ChatRoom() {
     const socket = useContext(ConnectionContext);
     const [message,setMessage] = useState([]);
-    const [newMessage,setNewMessage] = useState('')
 
     useEffect(()=>{
       socket.on('message',(data)=>{
@@ -15,28 +15,15 @@ function ChatRoom() {
       return ()=> socket.off('message')
     },[socket,message])
  
-    const sendMessage =(e)=>{
-        e.preventDefault();
-        socket.emit('newMessage',newMessage)
-        setNewMessage('')
-    }
-
   return (
     <div>
          <h2>Messages</h2>
+         
         <ul>
           {message.map(msg=><Message message={msg} key={msg.id}/>)}
         </ul>
-        <form onSubmit={sendMessage}>
-          <input 
-           type="text" 
-           value={newMessage}
-           onChange={(e)=>setNewMessage(e.target.value)}
-           placeholder='new message'
-           required
-          />
-         <button>Send</button>
-        </form>
+
+         <NewMessageForm />
     </div>
   )
 }
