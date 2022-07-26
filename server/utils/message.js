bc3f0f8c0700575850b5a010eb6decb62527c41c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-26 10:10:49 +0300

    add userId in returned message to enable styling

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,9 @@
 const {v4:uuid} = require('uuid')
 
-const generateMessage=(msg,username='Admin')=>{
+const generateMessage=(msg,username='Admin',socketId='admin')=>{
     return {
         id:uuid(),
+        userId:socketId,
         username,
         msg,
         createdAt:new Date().getTime()
