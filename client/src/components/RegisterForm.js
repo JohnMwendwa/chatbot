7cadc1d2534d6c747843be3c43643831dad6ce41
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-26 15:34:37 +0300

    import RegisterForm stylesheet and add a form title

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,6 +1,7 @@
 import React, { useState,useContext} from 'react';
 import { useNavigate } from 'react-router-dom';
 import {ConnectionContext} from '../context/connectionContext.js';
+import '../assets/css/RegisterForm.css'
 
 function RegisterForm() {
     const socket = useContext(ConnectionContext);
@@ -9,32 +10,45 @@ function RegisterForm() {
     const [errorMessage,setErrorMessage] = useState('');
     const navigate = useNavigate();
 
-    const handleSubmit =(e)=>{
+    const handleSubmit = (e)=>{
         e.preventDefault();
-        socket.emit('join',{username,room},(error)=>{
-            setErrorMessage(error)
-        })
-        navigate('/chat')
+       socket.emit('join',{username,room},(error)=>{
+
+            if(error){
+              return setErrorMessage(error)
+            }
+            navigate('/chat')
+            })              
     }
+    
   return (
 <div className='RegisterForm'>
+  <h1 className='RegisterForm__title'>Chatbot</h1>
+
    {errorMessage && <p className='RegisterForm__error'>{errorMessage}</p>}
+   
     <form onSubmit={handleSubmit} className='RegisterForm__form'>
+
+      <label htmlFor="username">username</label>
         <input 
-        type="text" 
-        value={username}
-        onChange={(e)=>setUsername(e.target.value)}
-        placeholder='username'
-        required
-        className='RegisterForm__username'
+            id='username'
+            type="text" 
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
+            placeholder='john'
+            required
+            className='RegisterForm__username'
         />
+
+        <label htmlFor="room">Room name</label>
         <input
-         type="text"
-         value={room}
-         onChange={(e)=>setRoom(e.target.value)}
-         placeholder='Room'
-         required
-         className='RegisterForm__room'
+            id='room'
+            type="text"
+            value={room}
+            onChange={(e)=>setRoom(e.target.value)}
+            placeholder='javascript'
+            required
+            className='RegisterForm__room'
          />
 
          <button className='RegisterForm__btn'>Join</button>
