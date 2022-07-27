0c6142d70fec59b36c7683a52821161d22aaed21
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-27 19:21:28 +0300

    listen for when a client disconnects

--- client/src/components/ChatRoom.js
+++ client/src/components/ChatRoom.js
@@ -1,20 +1,34 @@
 import {useEffect,useState,useContext} from 'react';
+import {useNavigate} from 'react-router-dom'
 import {ConnectionContext} from '../context/connectionContext.js';
 import Message from './Message.js';
 import NewMessageForm from './NewMessageForm.js';
 import '../assets/css/ChatRoom.css'
 
 function ChatRoom() {
-    const socket = useContext(ConnectionContext);
+    const {socket,isConnected,setIsConnected} = useContext(ConnectionContext);
     const [message,setMessage] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(()=>{
-      socket.on('message',(data)=>{
-        setMessage([...message,data])
+      if(isConnected){
+        socket.on('message',(data)=>{
+          setMessage([...message,data])
+        })
+      }
+
+      socket.on('disconnect',()=>{
+        setIsConnected(false)
+        alert("You've disconnected!")
+        navigate('/')
       })
 
-      return ()=> socket.off('message')
-    },[socket,message])
+      return ()=> {
+        socket.off('message')
+        socket.off('disconnect')
+      }
+    },[socket,message,isConnected,navigate,setIsConnected])
+    
   return (
     <div className='Chatroom'>
          <h2>Messages</h2>
