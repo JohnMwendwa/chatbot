bf51eb00e366dac2518afeb7cb3fe5049051bffe
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-28 01:01:35 +0300

    create state to track which room the client is logged into

--- client/src/context/connectionContext.js
+++ client/src/context/connectionContext.js
@@ -7,8 +7,9 @@ export const ConnectionContext = createContext()
 
 export function ConnectionProvider(props){
     const [isConnected,setIsConnected] = useState(socket.connected)
+    const [roomName,setRoomName] = useState('')
     return (
-    <ConnectionContext.Provider value={{socket,isConnected,setIsConnected}}>
+    <ConnectionContext.Provider value={{socket,isConnected,setIsConnected,roomName,setRoomName}}>
         {props.children}
     </ConnectionContext.Provider> 
     )
