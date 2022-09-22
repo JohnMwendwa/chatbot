aaf87cf8f5b14881f45b0d5965d0137da39d9833
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-23 00:42:45 +0300

    add a container wrapper for the login form

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,63 +1,62 @@
-import React, { useState,useContext} from 'react';
-import { useNavigate } from 'react-router-dom';
-import {ConnectionContext} from '../context/connectionContext.js';
-import '../assets/css/RegisterForm.css'
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { ConnectionContext } from "../context/connectionContext.js";
+import "../assets/css/RegisterForm.css";
 
 function RegisterForm() {
-    const {socket,setIsConnected,setRoomName} = useContext(ConnectionContext);
-    const [username,setUsername] = useState('');
-    const [room,setRoom] = useState('');
-    const navigate = useNavigate();
+  const { socket, setIsConnected, setRoomName } = useContext(ConnectionContext);
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-       socket.emit('join',{username,room},(error)=>{
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("join", { username, room }, (error) => {
+      if (error) {
+        alert(error);
+        navigate("/");
+      }
+    });
+    setIsConnected(true);
+    setRoomName(room);
+    navigate("/chat");
+  };
 
-            if(error){
-              alert(error)
-              navigate('/')
-            }
-           
-     })    
-        setIsConnected(true)
-        setRoomName(room)
-        navigate('/chat')          
-    }
-    
   return (
-<div className='RegisterForm'>
-  <h1 className='RegisterForm__title'>Chatbot</h1>
-{/* 
+    <div className="Container">
+      <div className="RegisterForm">
+        <h1 className="RegisterForm__title">Chatbot</h1>
+        {/* 
    {errorMessage && <p className='RegisterForm__error'>{errorMessage}</p>} */}
-   
-    <form onSubmit={handleSubmit} className='RegisterForm__form'>
 
-      <label htmlFor="username">username</label>
-        <input 
-            id='username'
-            type="text" 
+        <form onSubmit={handleSubmit} className="RegisterForm__form">
+          <label htmlFor="username">username</label>
+          <input
+            id="username"
+            type="text"
             value={username}
-            onChange={(e)=>setUsername(e.target.value)}
-            placeholder='john'
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="john"
             required
-            className='RegisterForm__username'
-        />
+            className="RegisterForm__username"
+          />
 
-        <label htmlFor="room">Room name</label>
-        <input
-            id='room'
+          <label htmlFor="room">Room name</label>
+          <input
+            id="room"
             type="text"
             value={room}
-            onChange={(e)=>setRoom(e.target.value)}
-            placeholder='javascript'
+            onChange={(e) => setRoom(e.target.value)}
+            placeholder="javascript"
             required
-            className='RegisterForm__room'
-         />
+            className="RegisterForm__room"
+          />
 
-         <button className='RegisterForm__btn'>Join</button>
-    </form>
-</div>
-  )
+          <button className="RegisterForm__btn">Join</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default RegisterForm
+export default RegisterForm;
